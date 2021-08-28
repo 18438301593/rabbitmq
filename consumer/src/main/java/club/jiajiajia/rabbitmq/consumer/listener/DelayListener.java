565535ea0ab819287468ad0e27b5daeccba63304
贾佳佳 <18438301593@163.com>
2021-08-28 13:00:38 +0800

    延时队列配置 导致启动报错

--- consumer/src/main/java/club/jiajiajia/rabbitmq/consumer/listener/DelayListener.java
+++ consumer/src/main/java/club/jiajiajia/rabbitmq/consumer/listener/DelayListener.java
@@ -2,7 +2,9 @@ package club.jiajiajia.rabbitmq.consumer.listener;
 
 import club.jiajiajia.rabbitmq.common.DelayExchangeConfig;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -14,8 +16,11 @@ import java.util.Map;
 * @Description: 延迟交换机 监听器
 */
 @Component
-@ConditionalOnBean(DelayExchangeConfig.class)
+@ConditionalOnProperty(name = "spring.delay", havingValue = "true")
 public class DelayListener {
+    public DelayListener(){
+        System.out.println("DelayListener init ");
+    }
     @RabbitListener(queues = "delayQueue")
     public void receive(Map map){
         System.out.println("delayQueue监听接收到消息:"+map);
